4c20c9e1398d14628b98b858bf12d66963046ac8
Jeyna Kim <jeyna.kimm@gmail.com>
2022-06-27 23:26:57 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -22,3 +22,10 @@ puts "----------"
 # Ask the user for a store name (store it in a variable)
 # Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
 # Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+puts "Enter a store name: "
+input: = gets.chomp
+new_store = Store.create(name: input)
+puts new_store.valid?
+puts new_store.errors.messages
+
